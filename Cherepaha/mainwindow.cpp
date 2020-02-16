0ef0063204b1285dd0e7a6b2a0d4472c47d26626
dimab0nd <36567568+dimab0nd@users.noreply.github.com>
2020-02-16 03:45:10 +0300

    Add basic UI

--- Cherepaha/mainwindow.cpp
+++ Cherepaha/mainwindow.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QFileDialog>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +16,23 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QString filter = "Text File (*.txt)";
+    QString file_name = QFileDialog::getOpenFileName(this,"open a file","C://",filter);
+
+    QFile file(file_name);
+    if (!file.open(QFile::ReadOnly | QFile::Text))
+    {
+        QMessageBox::warning(this,"warning","file not open!");
+     }
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->plainTextEdit->setPlainText(text);
+    file.close();
+}
