053889899b9eee22528e9422dfaaa1f719a1e0bf
dimab0nd <36567568+dimab0nd@users.noreply.github.com>
2020-02-17 04:23:34 +0300

    Add cherepaha.h ^-^

---
+++ Cherepaha/cherepaha.h
@@ -0,0 +1,74 @@
+#ifndef CHEREPAHA_H
+#define CHEREPAHA_H
+#include <utility>
+
+/**
+ * Шаблонный класс чрепахи
+ */
+enum TurnDiraction {L,R};
+enum StepDiraction {F,B};
+enum Orientation   {Up, Left, Right, Down};
+
+class cherepaha {
+public:
+    /**
+     * Создаё
+     */
+    cherepaha()
+        {
+        pos = std::make_pair(0,0);
+        CurrentOrientation = Up;
+            };
+    std::pair<int,int> GetCurrentPos()
+        {
+          return pos;
+        };
+    Orientation GetCurrentOrientation()
+    {
+        return CurrentOrientation;
+    };
+
+    void Turn(TurnDiraction& Dir)
+    {
+        switch ( Dir ) {
+        case L:
+         if(CurrentOrientation == Up) CurrentOrientation = Left;
+         else CurrentOrientation = Orientation((CurrentOrientation - 1 )%4 );
+
+
+          break;
+        case R:
+            CurrentOrientation = Orientation((CurrentOrientation + 1 )%4 );
+
+          break;
+        }
+    }
+    void Step(StepDiraction& Dir)
+    {
+        switch ( Dir ) {
+        case F:
+         switch(CurrentOrientation)
+         {
+         case Up: pos.second++; break;
+         case Left: pos.first--; break;
+         case Right: pos.first++; break;
+         case Down: pos.second--; break;
+         }
+          break;
+        case B:
+            switch(CurrentOrientation)
+            {
+            case Up: pos.second--; break;
+            case Left: pos.first++; break;
+            case Right: pos.first--; break;
+            case Down: pos.second++; break;
+            }
+          break;
+        }
+    }
+
+protected:
+    std::pair<int,int> pos;
+    Orientation CurrentOrientation;
+};
+#endif // CHEREPAHA_H
